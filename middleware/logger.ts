f4e5fea59398bd043c3ab5ab274210733adfedf3
Czino <eric@czino.de>
2023-07-13 09:35:12 +0200

    Add logger middleware

---
+++ middleware/logger.ts
@@ -0,0 +1,12 @@
+import morgan from 'morgan'
+import getLogger from '../src/utils/logger'
+
+const logger = getLogger('controller', 'All')
+
+// Override the stream method by telling
+// Morgan to use our custom logger instead of the console.log.
+const stream = {
+  write: (message: any) => logger.http(message),
+}
+
+export default morgan(':method :url :status :res[content-length] - :response-time ms', { stream })
