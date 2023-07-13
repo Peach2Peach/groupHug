a7626cece87b850a8b88342324bcdde72ec08393
Czino <eric@czino.de>
2023-07-13 16:32:38 +0200

    Add round utils

--- src/utils/math/index.ts
+++ src/utils/math/index.ts
@@ -1,1 +1,2 @@
+export { round } from './round'
 export { sum } from './sum'
---
+++ src/utils/math/round.ts
@@ -0,0 +1,5 @@
+const base10 = 10
+export const round = (num: number, digits = 0) => {
+  const exp = base10 ** digits
+  return Math.round(num * exp) / exp
+}
