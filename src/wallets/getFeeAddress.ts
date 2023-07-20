515afe8b2cc293bcf67cdd720518f0b360a1af18
Czino <eric@czino.de>
2023-07-20 15:00:48 +0200

    Add method to get fee address

---
+++ src/wallets/getFeeAddress.ts
@@ -0,0 +1,12 @@
+import { payments } from 'bitcoinjs-lib'
+import { feeWallet } from './feeWallet'
+import { NETWORK } from '../../constants'
+
+export const getFeeAddress = (index: number) => {
+  const feeCollector = feeWallet.derivePath(`0/${index}`)
+
+  return payments.p2wpkh({
+    pubkey: feeCollector.publicKey,
+    network: NETWORK,
+  }).address
+}
