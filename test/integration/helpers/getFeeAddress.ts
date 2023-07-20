aafb0c20af8bdd9c8010670df43f624b3abe5e51
Czino <eric@czino.de>
2023-07-20 15:01:23 +0200

    Add helper to get fee address

---
+++ test/integration/helpers/getFeeAddress.ts
@@ -0,0 +1,12 @@
+import { payments } from 'bitcoinjs-lib'
+import { NETWORK } from '../../../constants'
+import { feeWallet } from '../../../src/wallets'
+
+export const getFeeAddress = () => {
+  const feeCollector = feeWallet.derivePath('0/0')
+
+  return payments.p2wpkh({
+    pubkey: feeCollector.publicKey,
+    network: NETWORK,
+  }).address
+}
