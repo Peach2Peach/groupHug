f4892e59d319d9592c58205ecf1c7ef5def37bd6
Czino <eric@czino.de>
2023-07-20 19:09:38 +0200

    Can be string or number

--- src/wallets/getDerivationPathByIndex.ts
+++ src/wallets/getDerivationPathByIndex.ts
@@ -1,5 +1,5 @@
 import { Network } from 'bitcoinjs-lib'
 import { isTestnet } from '../utils/bitcoin'
 
-export const getDerivationPathByIndex = (index: number, network: Network) =>
+export const getDerivationPathByIndex = (index: number | string, network: Network) =>
   `m/48'/${isTestnet(network) ? '1' : '0'}'/0'/0/${index}`
--- src/wallets/getSignerByIndex.ts
+++ src/wallets/getSignerByIndex.ts
@@ -2,5 +2,5 @@ import { Network } from 'bitcoinjs-lib'
 import { getDerivationPathByIndex } from './getDerivationPathByIndex'
 import { BIP32Interface } from 'bip32'
 
-export const getSignerByIndex = (hdWallet: BIP32Interface, index: number, network: Network) =>
+export const getSignerByIndex = (hdWallet: BIP32Interface, index: number | string, network: Network) =>
   hdWallet.derivePath(getDerivationPathByIndex(index, network))
