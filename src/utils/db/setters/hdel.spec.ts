33ddf9aae0e721b312a8004bf727c48ad072c1f5
Czino <eric@czino.de>
2023-08-09 15:13:40 +0200

    Minor performance gain

--- src/utils/db/setters/hdel.spec.ts
+++ src/utils/db/setters/hdel.spec.ts
@@ -6,8 +6,10 @@ import { complexVal } from '../../../../test/data/primitiveData'
 describe('hdel', () => {
   it('should delete a subkey from a hash set', async () => {
     await db.transaction(async (client) => {
-      await client.hset('test-hm-key', complexVal)
-      await client.hdel('test-hm-key', 'subKey')
+      await Promise.all([
+        client.hset('test-hm-key', complexVal),
+        client.hdel('test-hm-key', 'subKey'),
+      ])
     })
     expect(await db.hgetall('test-hm-key')).to.not.have.key('subKey')
   })
