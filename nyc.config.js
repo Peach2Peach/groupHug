08a3286f184579115080d83bd7ff7cd8d0b2d2ce
Czino <eric@czino.de>
2023-08-09 16:49:13 +0200

    Coverage on CI is different

--- nyc.config.js
+++ nyc.config.js
@@ -2,8 +2,8 @@ module.exports = {
   exclude: ['**/*.spec.ts', 'test/**/*', 'constants.ts'],
   'check-coverage': true,
   reporter: 'html',
-  statements: 99.55,
-  branches: 97.16,
+  statements: 99.47,
+  branches: 96.69,
   functions: 96.8,
-  lines: 99.59,
+  lines: 99.49,
 }
