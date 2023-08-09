432dd75f3efb7694152dcf550820c9ca6c773d43
Czino <eric@czino.de>
2023-08-09 11:52:42 +0200

    Add cache control of 60 seconds

--- controllers/batch/index.ts
+++ controllers/batch/index.ts
@@ -1,4 +1,5 @@
 import { Express } from 'express'
+import { addResponseHeaders } from '../../middleware/addResponseHeaders'
 import getLogger from '../../src/utils/logger'
 import { getBatchStatusController } from './getBatchStatusController'
 import { validateGetBatchStatus } from './validation'
@@ -6,7 +7,14 @@ import { validateGetBatchStatus } from './validation'
 const serverLogger = getLogger('server', 'log')
 
 export const Batch = (app: Express): void => {
-  app.get('/v1/batch', validateGetBatchStatus, getBatchStatusController)
+  app.get(
+    '/v1/batch',
+    addResponseHeaders({
+      'Cache-control': 'public, max-age=60',
+    }),
+    validateGetBatchStatus,
+    getBatchStatusController,
+  )
 
   serverLogger.info('Installed batch endpoints')
 }
