7ce2483b882085471fced5ac9560e6db544b9ce1
Michael Manke <66833819+MManke188@users.noreply.github.com>
2024-05-14 02:15:19 +0200

    PP-3004: Removed more unused code

--- test/unit/helpers/spiceTxWithPSBTInput.ts
+++
@@ -1,8 +0,0 @@
-import { PsbtTxInput } from "bitcoinjs-lib";
-import blockExplorerData from "../../data/blockExplorerData.json";
-import { getTxIdOfInput } from "../../../src/utils/psbt";
-
-export const spiceTxWithPSBTInput = (input: PsbtTxInput): Transaction => ({
-  ...blockExplorerData.tx,
-  txid: getTxIdOfInput(input),
-});
