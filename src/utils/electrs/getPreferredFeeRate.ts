82c25c08f5ee8c4012bb727b059ded9a7c653e93
Michael Manke <michael@peachbitcoin.com>
2025-06-02 18:20:58 +0200

    Switched to esplora again for regtest only

--- src/utils/electrs/getPreferredFeeRate.ts
+++ src/utils/electrs/getPreferredFeeRate.ts
@@ -1,10 +1,22 @@
-import { MEMPOOL_URL } from "../../../constants";
+import { networks } from "bitcoinjs-lib";
+import { BLOCKEXPLORERURL, MEMPOOL_URL, NETWORK } from "../../../constants";
 import fetch from "../../../middleware/fetch";
 import getLogger from "../logger";
 
 const logger = getLogger("fetch", "getPreferredFeeRate");
-export const getPreferredFeeRate = () =>
-  fetch(`${MEMPOOL_URL}/v1/fees/recommended`)
+export const getPreferredFeeRate = () => {
+  if (NETWORK === networks.regtest) {
+    return fetch(`${BLOCKEXPLORERURL}/fee-estimates`)
+      .then(async (response) => {
+        const electrsRes = await response.json();
+        return electrsRes["6"];
+      })
+      .catch((err) => {
+        logger.error(["Could not get fee estimates", err]);
+        return null;
+      });
+  }
+  return fetch(`${MEMPOOL_URL}/v1/fees/recommended`)
     .then(async (response) => {
       const { hourFee } = await response.json();
       return hourFee as number;
@@ -13,3 +25,4 @@ export const getPreferredFeeRate = () =>
       logger.error(["Could not get fee estimates", err]);
       return null;
     });
+};
